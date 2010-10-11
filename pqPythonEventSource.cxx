6083646be16cb3bc236f95e908dbc6dcc2a61427
David Partyka <dave.partyka@kitware.com>
2010-10-11 15:55:34 -0400

    ENH: Apply changes to build QtTesting standalone.

--- pqPythonEventSource.cxx
+++ pqPythonEventSource.cxx
@@ -30,9 +30,30 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =========================================================================*/
 
-// python header first
-// TODO:  Fix this so we don't depend on VTK
-#include "vtkPython.h"
+/*
+   Use the "documented" trick involving checking for _DEBUG
+   and undefined that symbol while we include Python headers.
+   Update: this method does not fool Microsoft Visual C++ 8 anymore; two
+   of its header files (crtdefs.h and use_ansi.h) check if _DEBUG was set
+   or not, and set flags accordingly (_CRT_MANIFEST_RETAIL,
+   _CRT_MANIFEST_DEBUG, _CRT_MANIFEST_INCONSISTENT). The next time the
+   check is performed in the same compilation unit, and the flags are found,
+   and error is triggered. Let's prevent that by setting _CRT_NOFORCE_MANIFEST.
+*/
+
+#ifdef _DEBUG
+# undef _DEBUG
+# if defined(_MSC_VER) && _MSC_VER >= 1400
+#   define _CRT_NOFORCE_MANIFEST 1
+# endif
+# include <Python.h>
+# define _DEBUG
+#else
+# include <Python.h>
+#endif
+
+
+
 #ifndef PyMODINIT_FUNC
 #define PyMODINIT_FUNC extern "C" void
 #endif // PyMODINIT_FUNC
