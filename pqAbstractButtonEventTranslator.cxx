e75fc1466fba7c51a139be3d2ce8a05276d42c03
Benjamin Long <benjamin.long@kitware.com>
2012-03-20 11:54:30 -0400

    Fix event record under a QToolButton.
    Now if the QToolButton get only one QAction, we record a click.
    We do that in case the signal clicked() is connect to a slot.

--- pqAbstractButtonEventTranslator.cxx
+++ pqAbstractButtonEventTranslator.cxx
@@ -124,6 +124,10 @@ void pqAbstractButtonEventTranslator::onActivate(QAbstractButton* actualObject)
     const bool new_value = !actualObject->isChecked();
     emit recordEvent(object, "set_boolean", new_value ? "true" : "false");
     }
+  else if(tb && tb->actions().count() < 2)
+    {
+    emit recordEvent(tb, "activate", "");
+    }
   else
     {
     emit recordEvent(object, "activate", "");
