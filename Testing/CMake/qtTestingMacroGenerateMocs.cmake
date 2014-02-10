150a3fe9760fff4535b6e7b80baed42fa37fdf7c
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2014-02-10 10:23:08 -0500

    Qt5 support for QtTesting master
    Qt5 is built and packaged with the -reduce-relocations configure option as
    default. This causes compilations to run with the -Bsymbolic-functions option,
    which makes function pointer comparison ineffective, unless the -fPIE flag is
    also supplied. Added Qt5 compile flags to support position independent code.
    
    Replaced all instances QString::toAscii to QString::toLatin1

--- Testing/CMake/qtTestingMacroGenerateMocs.cmake
+++ Testing/CMake/qtTestingMacroGenerateMocs.cmake
@@ -9,3 +9,13 @@ macro(QT4_GENERATE_MOCS)
   endforeach()
 endmacro()
 
+# QT5_GENERATE_MOCS(inputfile1 [inputfile2 ...])
+
+macro(QT5_GENERATE_MOCS)
+  foreach(file ${ARGN})
+    set(moc_file moc_${file})
+    QT5_GENERATE_MOC(${file} ${moc_file})
+    set_property(SOURCE ${file} APPEND PROPERTY
+      OBJECT_DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${moc_file})
+  endforeach()
+endmacro()
